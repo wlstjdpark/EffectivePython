45e42ea4e2a4b2ae8c77cb8b55f6928cae5887a3
wlstjdpark <wlstjdpark@gmail.com>
2016-09-20 00:04:17 +0900

    complete to write source code

--- Chapter7/Chapter7.py
+++ Chapter7/Chapter7.py
@@ -1,3 +1,21 @@
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = [x**2 for x in a]
 print(squares)
+
+squares = map(lambda x: x ** 2, a)
+print(list(squares))
+
+even_squares = [x**2 for x in a if x % 2 == 0]
+print(even_squares)
+
+alt = map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, a))
+print(list(alt))
+
+chile_ranks = {'ghost': 1, 'habanero': 2, 'cayenne': 3}
+rank_dict = {rank: name for name, rank in chile_ranks.items()}
+chile_len_set = {len(name) for name in chile_ranks}
+print(rank_dict)
+print(chile_len_set)
+
+
+
