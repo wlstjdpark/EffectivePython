9e27e5ff4ffe8eac8d8453e5388fd5287ee9fa7a
wlstjdpark <wlstjdpark@gmail.com>
2016-09-21 00:42:12 +0900

    modified multiplex list comprehension example

--- Chapter8/Chapter8.py
+++ Chapter8/Chapter8.py
@@ -30,6 +30,6 @@ c = [x for x in a if x > 4 and x % 2 == 0]
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-filtered = [x for row in matrix if sum(row) >= 10
-            for x in row if x % 3 == 0]
+filtered = [[x for x in row if x % 3 == 0]
+            for row in matrix if sum(row) >= 10]
 print(filtered)
