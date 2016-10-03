55cf92a2787e4397393241bf8426bd591056a8cb
wlstjdpark <wlstjdpark@gmail.com>
2016-10-03 22:00:54 +0900

    empty

---
+++ Chapter3/Chapter3.py
@@ -0,0 +1,34 @@
+
+
+def to_str(bytes_or_str):
+    if isinstance(bytes_or_str, bytes):
+        value = bytes_or_str.decode('utf-8')
+    else:
+        value = bytes_or_str
+    return value  # str 인스턴스
+
+
+def to_bytes(bytes_or_str):
+    if isinstance(bytes_or_str, str):
+        value = bytes_or_str.encode('utf-8')
+    else:
+        value = bytes_or_str
+    return value  # bytes 인스턴스
+
+'''
+# 파이썬 2
+def to_unicode(unicode_or_str):
+    if isinstance(unicode_or_str, str):
+        value = unicode_or_str.decode('utf-8')
+    else:
+        value = unicode_or_str
+    return value  # unicode 인스턴스
+
+def to_str(unicode_or_str):
+    if isinstance(unicode_or_str, unicode):
+        value = unicode_or_str.encode('utf-8')
+    else:
+        value = unicode_or_str
+    return value  # str 인스턴스
+'''
+
